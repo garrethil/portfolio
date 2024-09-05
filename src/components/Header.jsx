241992cf7bd3f604823506240fe1e07c663d75ba
garrethil <garrethildebrandt@gmail.com>
2024-09-05 13:14:33 -0400

    correct routing format for a SPA and for NAV bar to operate for id-selection without changing the endpoint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,30 @@ import { useState } from "react";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleScrollToSection = (e, sectionId) => {
+    e.preventDefault(); // Prevent the default anchor link behavior
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" }); // Scroll to the section smoothly
+    }
+  };
+
   return (
     <header className="bg-primary text-primary-text font-sans">
       <div className="container mx-auto flex justify-between items-center p-5 md:p-8 lg:p-10">
         <div className="text-2xl lg:text-[35px] font-bold items-center justify-center">
           Garret Hildebrandt{" "}
-          {/* Show "Full-Stack Developer" only on larger screens */}
           <span className="hidden lg:inline font-semibold underline ml-3">
             - Full-Stack Developer
           </span>
         </div>
         <nav className="hidden text-[20px] lg:text-[30px] md:flex space-x-4">
-          <a href="#projects" className="hover:text-gray-400">
+          {/* Use onClick handler to prevent default and manually scroll */}
+          <a
+            href="#projects"
+            onClick={(e) => handleScrollToSection(e, "projects")}
+            className="hover:text-gray-400"
+          >
             Projects
           </a>
         </nav>
@@ -42,7 +54,11 @@ const Header = () => {
       </div>
       {isOpen && (
         <nav className="md:hidden bg-gray-800 p-5">
-          <a href="#projects" className="hover:text-gray-400">
+          <a
+            href="#projects"
+            onClick={(e) => handleScrollToSection(e, "projects")}
+            className="hover:text-gray-400"
+          >
             Projects
           </a>
         </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,18 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 import Home from "./pages/Home.jsx";
 import Error from "./pages/Error.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  </HashRouter>
 );
