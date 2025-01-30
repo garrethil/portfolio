f2b501e5ad6b2e7ef50ca31ccdfffdcc9cd59bc8
garrethil <garrethildebrandt@gmail.com>
2025-01-30 13:50:19 -0500

    add resume to home page links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+const resumePath = "/hildebrandt.25.pdf";
+
 function Home() {
   return (
     <div className="w-full bg-primary flex flex-col items-center justify-center h-screen px-4 sm:px-6 md:px-8 lg:px-10">
@@ -68,6 +70,25 @@ function Home() {
                 <g id="mail">
                   <path d="M29,6H3L2.92,6a.78.78,0,0,0-.21,0l-.17.07a.65.65,0,0,0-.15.1.67.67,0,0,0-.15.14l-.06.06a.36.36,0,0,0,0,.09,1.08,1.08,0,0,0-.08.19A1.29,1.29,0,0,0,2,6.9S2,7,2,7V25a1,1,0,0,0,1,1H29a1,1,0,0,0,1-1V7A1,1,0,0,0,29,6ZM16,14.81,6.2,8H27.09ZM4,24V8.91l11.43,7.91,0,0a1.51,1.51,0,0,0,.18.09l.08,0A1.09,1.09,0,0,0,16,17h0a1,1,0,0,0,.41-.1l.07,0,0,0L28,9.79V24Z" />
                 </g>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href={resumePath}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-highlight hover:underline transition duration-300"
+              name="resume preview"
+              aria-label="view resume"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-9 w-9 md:h-11 md:w-11 lg:h-15 lg:w-15 hover:-translate-y-1 duration-800 transition-transform transform"
+                viewBox="0 0 24 24"
+                fill="#85ae8e"
+              >
+                <path d="M14 2H6C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V8L14 2ZM14 3.5L18.5 8H14V3.5ZM7 11H17V13H7V11ZM7 15H17V17H7V15Z" />
               </svg>
             </a>
           </li>
